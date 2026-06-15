78dc15dd8092b39f3bda7c05601dad9042c6971b
JuliusBourbon <naherrrrr@gmail.com>
2026-06-15 21:54:46 +0700

    fix: api base url

--- seapedia-fe/src/constants/config.ts
+++ seapedia-fe/src/constants/config.ts
@@ -1,10 +1,6 @@
 import { Platform } from 'react-native';
 
-export const API_BASE_URL = Platform.select({
-  android: 'http://10.0.2.2:3000/api/v1',
-  ios: 'http://localhost:3000/api/v1',
-  default: 'http://localhost:3000/api/v1',
-});
+export const API_BASE_URL = "http://192.168.1.11:3000/api/v1";
 
 export const DELIVERY_METHODS = {
   INSTANT: {
